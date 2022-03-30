43addcfa28c0492817ad603fe6761f3630f03fec
emma.malm@solita.se <emma.malm@solita.se>
2022-03-30 19:42:51 +0000

    Test

---
+++ tests/assert_positive_value_for_total_amount.sql
@@ -0,0 +1,13 @@
+-- Refunds have a negative amount, so the total amount should always be >= 0.
+-- Therefore return records where this isn't true to make the test fail.
+with 
+
+payments as(
+    select * from {{ref('stg_payments')}}
+)
+select
+  order_id,
+	sum(amount) as total_amount
+from {{ ref('stg_payments') }}
+group by 1
+having not(total_amount >= 0)
